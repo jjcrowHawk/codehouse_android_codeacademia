9dace3d62a6039da614b5feb1f840e6e0a764fc8
jjcrowHawk <jjcrow@espol.edu.ec>
2019-07-18 13:05:13 -0500

    adding few changes

--- app/src/main/java/com/codehouse/codeacademia/Modelo/Anuncio.java
+++ app/src/main/java/com/codehouse/codeacademia/Modelo/Anuncio.java
@@ -2,6 +2,8 @@ package com.codehouse.codeacademia.Modelo;
 
 import android.graphics.drawable.Drawable;
 
+import java.util.ArrayList;
+
 public class Anuncio {
     private String titulo;
     private String descripcion;
@@ -36,5 +38,20 @@ public class Anuncio {
 
     public void setFecha(String fecha) {
         this.fecha = fecha;
+    }
+
+    public static ArrayList<Anuncio> inicializarAnunciosPrueba(){
+        String descripconGenerica= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis metus hendrerit, auctor libero ac, ornare lacus. Etiam eu fermentum nibh, sit amet consectetur nisi. Curabitur rhoncus et neque id gravida. Morbi mauris urna, tristique vel mollis at, faucibus sit amet risus. Proin vitae condimentum mauris. Nunc ullamcorper convallis pulvinar. Donec mattis nulla eget velit mattis convallis. Aliquam quis leo accumsan, sagittis libero quis, pharetra urna.";
+        ArrayList<Anuncio> anuncios= new ArrayList<>();
+
+        anuncios.add(new Anuncio("Taller de programacion en Android",descripconGenerica,"Julio 18, 2019"));
+        anuncios.add(new Anuncio("Taller de programacion en iOS",descripconGenerica,"Julio 15, 2019"));
+        anuncios.add(new Anuncio("Examen de POO",descripconGenerica,"Julio 10, 2019"));
+        anuncios.add(new Anuncio("Curso semanal de Base de Datos",descripconGenerica,"Julio 9, 2019"));
+        anuncios.add(new Anuncio("Seminario de IoT",descripconGenerica,"Julio 5, 2019"));
+        anuncios.add(new Anuncio("Tour a campus party",descripconGenerica,"Julio 3, 2019"));
+        anuncios.add(new Anuncio("Nueva serie de videos de Diseño Web",descripconGenerica,"Junio 18, 2019"));
+
+        return anuncios;
     }
 }
--- app/src/main/java/com/codehouse/codeacademia/Modelo/Tarea.java
+++ app/src/main/java/com/codehouse/codeacademia/Modelo/Tarea.java
@@ -1,5 +1,7 @@
 package com.codehouse.codeacademia.Modelo;
 
+import java.util.ArrayList;
+
 public class Tarea {
     private String nombre;
     private String descripcion;
@@ -43,5 +45,20 @@ public class Tarea {
 
     public void setPuntos(int puntos) {
         this.puntos = puntos;
+    }
+
+    public static ArrayList<Tarea>  incializarTareasPrueba(){
+        String descripconGenerica= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis metus hendrerit, auctor libero ac, ornare lacus. Etiam eu fermentum nibh, sit amet consectetur nisi. Curabitur rhoncus et neque id gravida. Morbi mauris urna, tristique vel mollis at, faucibus sit amet risus. Proin vitae condimentum mauris. Nunc ullamcorper convallis pulvinar. Donec mattis nulla eget velit mattis convallis. Aliquam quis leo accumsan, sagittis libero quis, pharetra urna.";
+        ArrayList<Tarea> tareas= new ArrayList<>();
+
+        tareas.add(new Tarea("Tarea de MVC",descripconGenerica,"Julio 25, 2019",10));
+        tareas.add(new Tarea("Investigacion de RecyclerView",descripconGenerica,"Julio 20, 2019",20));
+        tareas.add(new Tarea("Deber de patrones de diseño",descripconGenerica,"Julio 19, 2019",30));
+        tareas.add(new Tarea("Examen digital de POO",descripconGenerica,"Julio 18, 2019",50));
+        tareas.add(new Tarea("Lección digital de polimorfismo",descripconGenerica,"Julio 15, 2019",10));
+        tareas.add(new Tarea("Exposición sobre Arquitectura Android",descripconGenerica,"Julio 13, 2019",15));
+        tareas.add(new Tarea("Exposición de Arquitectura iOS",descripconGenerica,"Junio 18, 2019",10));
+
+        return tareas;
     }
 }
